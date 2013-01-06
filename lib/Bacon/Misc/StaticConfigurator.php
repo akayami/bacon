00e866e6e9904c631728c5839b7144eb8c2362d6
Tomasz Rakowski <akayami@gmail.com>
2013-01-06 07:46:06 -0500

    Added trait Static Configurator

---
+++ lib/Bacon/Misc/StaticConfigurator.php
@@ -0,0 +1,15 @@
+<?php
+namespace Bacon\Misc;
+
+Trait StaticConfigurator {
+
+	protected static $_config;
+
+	public static function setConfig(array $config) {
+		static::$_config = $config;
+	}
+
+	public function getConfig() {
+		return $this::$_config;
+	}
+}
