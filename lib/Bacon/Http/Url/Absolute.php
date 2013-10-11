1d23f0cc01ceec43760b4cf75096db4d0a8236a0
Tomasz Rakowski <tomasz.rakowski@manwin.com>
2013-10-11 11:09:30 -0400

    Strippig out query string on redirect from base url.

--- lib/Bacon/Http/Url/Absolute.php
+++ lib/Bacon/Http/Url/Absolute.php
@@ -6,7 +6,9 @@ class Absolute {
 	protected $base;
 	
 	public function __construct($base = '') {
-		$this->base = $base;
+		$parsed = parse_url($base);
+		unset($parsed['query']);
+		$this->base = http_build_query($parsed);
 	}
 	
 	public function rewrite($url) {
